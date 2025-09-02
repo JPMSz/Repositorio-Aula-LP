920f60f10bd10920855cd3adf99be5e880bcd0bd
JPMSz <jpmds16@gmail.com>
2025-09-02 10:36:15 -0300

    Commit de python

---
+++ teste.py
@@ -0,0 +1,8 @@
+n1 = (int(input("Digite o número de títulos mundiais do seu clube do coração no Brasil: ")))
+if n1 < 3: 
+    print(" Seu time tem menos mundiais que o São Paulo F.C.")
+elif n1 == 3: 
+    print(" Você torce para o São Paulo F.C.")
+elif n1 > 3: 
+    print(" Você está mentindo!")
+
